16fb46364afcd308e83efa4f243d7c0eef02ca46
AhmedTarek <ataboulfotouh@gmail.com>
2025-03-17 05:01:20 -0600

    adding manual seed to fix seed per run

--- main_finetune_positioning.py
+++ main_finetune_positioning.py
@@ -110,7 +110,6 @@ def get_args_parser():
                         help='path where to tensorboard log')
     parser.add_argument('--device', default='cuda',
                         help='device to use for training / testing')
-    parser.add_argument('--seed', default=0, type=int)
     parser.add_argument('--resume', default='',
                         help='resume from checkpoint')
 
@@ -255,7 +254,7 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir and (epoch % 20 == 0 or (epoch + 1) == args.epochs):
+        if args.output_dir and (epoch % 10 == 0 or (epoch + 1) == args.epochs):
             misc.save_model(
                 args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
                 loss_scaler=loss_scaler, epoch=epoch)
